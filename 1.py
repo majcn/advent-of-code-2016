638740f2cf4634bc1d8ecc405a57ce48eb651403
Gregor Majcen <majcn.m@gmail.com>
2016-12-01 15:45:10 +0100

    minify a bit

--- 1.py
+++ 1.py
@@ -6,14 +6,14 @@ data = [(x[0], int(x[1:])) for x in data]
 
 
 # 0 -> up; 1 -> right; 2 -> down; 3 -> left
-moveDict = {
+move = {
     0: lambda x, y: (x, y + 1),
     1: lambda x, y: (x + 1, y),
     2: lambda x, y: (x, y - 1),
     3: lambda x, y: (x - 1, y)
 }
 
-directions = {
+nextDirection = {
     0: { 'L': 3, 'R': 1},
     1: { 'L': 0, 'R': 2},
     2: { 'L': 1, 'R': 3},
@@ -23,19 +23,12 @@ directions = {
 locations = [(0, 0)]
 direction = 0
 for d, l in data:
-    direction = directions[direction][d]
+    direction = nextDirection[direction][d]
     for i in range(l):
-        locations.append(moveDict[direction](*locations[-1]))
+        locations.append(move[direction](*locations[-1]))
 
-def getVisitTwice(locations):
-    for i1, l1 in enumerate(locations):
-        for i2, l2 in enumerate(locations):
-            if (i1 != i2 and l1 == l2):
-                return l1
-    return None
-
-def getLocationLength(location):
-    return abs(location[0]) + abs(location[1])
-
-print getLocationLength(locations[-1])
-print getLocationLength(getVisitTwice(locations))
+result = [
+    locations[-1],
+    next(l for i, l in enumerate(locations) if l in locations[(i+1):])
+]
+print map(lambda x: abs(x[0]) + abs(x[1]), result)
