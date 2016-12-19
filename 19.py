7f02b6a98d2a119240c148a9dd238059ae1145d1
Gregor Majcen <majcn.m@gmail.com>
2016-12-19 19:06:37 +0100

    spaces

--- 19.py
+++ 19.py
@@ -11,17 +11,17 @@ for i in elfs:
 print r + 1
 
 def rob(elfs):
-	l = len(elfs)
-	robbed = set()
+    l = len(elfs)
+    robbed = set()
 
-	for i in xrange(l):
-		r1 = ((l - i) / 2) + (2 * i)
-		if r1 >= l:
-			fp = elfs[:i]
-			lp = [x for index, x in enumerate(elfs[i:]) if index + i not in robbed]
-			return lp + fp
-		robbed.add(r1)
+    for i in xrange(l):
+        r1 = ((l - i) / 2) + (2 * i)
+        if r1 >= l:
+            fp = elfs[:i]
+            lp = [x for index, x in enumerate(elfs[i:]) if index + i not in robbed]
+            return lp + fp
+        robbed.add(r1)
 
 while len(elfs) > 1:
-	elfs = rob(elfs)
+    elfs = rob(elfs)
 print elfs[0]
